3a7f570571678ccff2356dd91ff5066c4e37c5cc
朱正钱 <hachi.zzq@gmail.com>
2018-08-09 23:22:31 +0800

    修改归档方法

--- tar_storage.sh
+++ tar_storage.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 date=$(date -d '+0 days' +%Y%m%d)
 
-tar -jvcf "/share/$date.tar.bz2" /share/* && mv "/share/$date.tar.bz2" /oss/
+cd /share && tar -jvcf "/share/$date.tar.bz2" * && mv "/share/$date.tar.bz2" /oss/
