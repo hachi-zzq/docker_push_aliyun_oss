05654a3098e2233ed98c0e67c1bc8b9775509945
朱正钱 <hachi.zzq@gmail.com>
2018-08-18 23:56:43 +0800

    update tar shell

--- tar_storage.sh
+++ tar_storage.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
+
+dir="/oss/$(date -d '+0 hours' +%Y)/$(date -d '+0 hours' +%m)"
+
+if [ ! -d $dir ]; then
+	mkdir -p $dir
+fi
+
 date=$(date -d '+0 hours' +%Y%m%d%H%M%S)
 
-cd /share && tar -jvcf "/share/$date.tar.bz2" * && mv "/share/$date.tar.bz2" /oss/
+cd /share && tar -jvcf "/share/$date.tar.bz2" * && mv "/share/$date.tar.bz2" $dir
